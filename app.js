b8b9f6b3671d154e05ac64bf4a52535c081f9795
nickonyi <nickoluoch1130@gmail.com>
2025-11-12 15:06:15 +0300

    some code for node express

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello world"));
+app.get("/{*splat}", (req, res) => {
+  res.send(
+    "/{*splat} is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling."
+  );
+});
 
+app.get("/messages", (req, res) => {
+  res.send(
+    "This route will not be reached because the previous route's path matches first"
+  );
+});
 const PORT = 3000;
 
 app.listen(PORT, (err) => {
