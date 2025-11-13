44f7f75247167571f09a2c30c809ed040059af29
nickonyi <nickoluoch1130@gmail.com>
2025-11-13 19:40:41 +0300

    some express staff

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+console.log(app.router);
 
 app.get("/", (req, res) => res.send("Hello world"));
+app.get("/:username/messages", (req, res) => {
+  console.log("router parameters", req.params);
+  console.log("query parameters", req.query);
 
+  res.end();
+});
+
+app.get("/:username/messages/:sadam", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
 const PORT = 3000;
 
 app.listen(PORT, (err) => {
