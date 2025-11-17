6fe2cabb5284890ab452abb726f7584a1c093e6f
nickonyi <nickoluoch1130@gmail.com>
2025-11-17 13:46:52 +0300

    routes

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+app.use((req, res) => {
+  // This works and this ends the request-response cycle
+  res.send("Hello");
+
+  // However, it does not exit the function so this will still run
+  console.log("will still run!!");
+
+  // This will then throw an error that we cannot send again after sending to the client already
+  res.send("Bye");
+});
+
 const PORT = 3000;
 
 app.listen(PORT, (err) => {
