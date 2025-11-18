c23791a57053dc52ad45c93fbec77aee5335a812
nickonyi <nickoluoch1130@gmail.com>
2025-11-18 14:08:43 +0300

    express code

--- app.js
+++ app.js
@@ -4,20 +4,17 @@ const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
 
-app.use("/authors", authorRouter);
-app.use("/books", bookRouter);
-app.use("/", indexRouter);
-
 app.use((req, res) => {
   // This works and this ends the request-response cycle
   res.send("Hello");
 
   // However, it does not exit the function so this will still run
   console.log("will still run!!");
-
-  // This will then throw an error that we cannot send again after sending to the client already
-  res.send("Bye");
 });
+
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
 
 app.get("/messages", (req, res) => {
   res.send(
