df6deec9435c1a51b8557f1f8bbb8c8762eae723
nickonyi <nickoluoch1130@gmail.com>
2026-06-09 22:05:52 +0300

    reseted the repo

--- errors/CustomNotFoundError.js
+++
@@ -1,9 +0,0 @@
-class CustomNotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 404;
-    this.name = "NotFoundError";
-  }
-}
-
-module.exports = CustomNotFoundError;
--- routes/authorRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getAuthorById } = require("../controller/authController");
-
-const authorRouter = Router();
-
-authorRouter.get("/", (req, res) => res.send("all authors"));
-authorRouter.get("/:authorId", getAuthorById);
-
-module.exports = authorRouter;
