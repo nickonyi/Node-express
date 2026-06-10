06a27b99a79e2b6a80c3e0c5f7bec2961105030b
nickonyi <nickoluoch1130@gmail.com>
2026-06-10 22:40:47 +0300

    feat: implement routing and serve HTML pages for home, about, and contact

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from "express";
+import url from "url";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world!"));
+const __fileName = url.fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+
+console.log(__dirName);
+
+app.get("/", (req, res) => res.sendFile(__dirName + "/pages/index.html"));
+app.get("/about", (req, res) => res.sendFile(__dirName + "/pages/about.html"));
+app.get("/contact", (req, res) =>
+  res.sendFile(__dirName + "/pages/contact.html"),
+);
 
 const PORT = 3000;
 
