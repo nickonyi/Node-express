bda1611eeb240b6d54bd169907f2da8fc256a6c3
nickonyi <nickoluoch1130@gmail.com>
2026-06-17 22:33:53 +0300

    feat: implement firstToLast function to calculate distance between first and last occurrence of a character

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function firstToLast(str, c) {
   //coding here..
+
+  if (!str.includes(c)) {
+    return -1;
+  }
+
+  const first = str.indexOf(c);
+  const last = str.lastIndexOf(c);
+
+  return last - first;
 }
+
+console.log(firstToLast("ababc", "c"));
