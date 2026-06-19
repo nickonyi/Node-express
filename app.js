28d0557ffeac8e68786c5e186e9978cc902d6c6e
nickonyi <nickoluoch1130@gmail.com>
2026-06-19 20:45:30 +0300

    feat: refactor app.js to use routers for authors, books, and index routes

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 import express from "express";
-import url from "url";
-import path from "path";
+import indexRouter from "./routes/indexRouter.js";
+import bookRouter from "./routes/bookRouter.js";
+import authRouter from "./routes/authorRouter.js";
 
 const app = express();
 
-const __fileName = url.fileURLToPath(import.meta.url);
-const __dirName = path.dirname(__fileName);
-
-console.log(__dirName);
-
-app.get("/", (req, res) => res.sendFile(__dirName + "/pages/index.html"));
-app.get("/about", (req, res) => res.sendFile(__dirName + "/pages/about.html"));
-app.get("/contact", (req, res) =>
-  res.sendFile(__dirName + "/pages/contact.html"),
-);
+app.use("/authors", authRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
 
 const PORT = 3000;
 
