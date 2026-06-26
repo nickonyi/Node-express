2ef9cc65c0c4096929eb7d431112e4a019388b51
nickonyi <nickoluoch1130@gmail.com>
2026-06-26 23:46:17 +0300

    refactor: simplify author retrieval logic by removing unnecessary try-catch block

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -4,17 +4,12 @@ import CustomErrorNotFound from "../errors/CustomNotFoundError.js";
 const getAuthorById = async (req, res) => {
   const { authorId } = req.params;
 
-  try {
-    const author = await db.getAuthorById(Number(authorId));
-    if (!author) {
-      throw new CustomErrorNotFound("Author not found!!!");
-    }
-
-    res.send(`Author name:${author.name}`);
-  } catch (err) {
-    console.error("error retrieving author", err);
-    res.status(500).send("Internal server error");
+  const author = await db.getAuthorById(Number(authorId));
+  if (!author) {
+    throw new CustomErrorNotFound("Author not found!!!");
   }
+
+  res.send(`Author name:${author.name}`);
 };
 
 export default getAuthorById;
