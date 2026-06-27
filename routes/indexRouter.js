315fd72336c9f2dbe4d14ae654b08ee30aae85d4
nickonyi <nickoluoch1130@gmail.com>
2026-06-27 17:52:27 +0300

    feat: implement EJS view rendering with navbar and user list

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,15 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 indexRouter.get("/", (req, res) =>
-  res.render("index", { message: "Ejs rocks!!!" }),
+  res.render("index", { links: links, users: users }),
 );
 indexRouter.get("/about", (req, res) => res.send("About page"));
 indexRouter.get("/contact", (req, res) => res.send("Contact page"));
